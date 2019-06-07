2e3e473e7f51a7d3bc58466ca05e43264a25765c
Tim Warnock <tim@timwarnock.com>
2019-06-06 23:59:03 -0700

    added Japanese examples

---
+++ test_j_set.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 tabstop=4 shiftwidth=4 autoindent smartindent:
+import wordscanner as ws
+
+
+if __name__== "__main__":
+    grid = ws.load_grid('jgrid10k.txt')
+    jp = ws.load_words('日本.txt')
+    matches = ws.find_words(ws._setscan_rtl(grid,jp), jp)
+    print(len(matches))
