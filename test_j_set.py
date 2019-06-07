f50307dbad908cb3b68c8d4cd3e617b58822ece9
Tim Warnock <tim@timwarnock.com>
2019-06-07 00:05:54 -0700

    added Japanese examples

--- test_j_set.py
+++ test_j_set.py
@@ -4,7 +4,7 @@ import wordscanner as ws
 
 
 if __name__== "__main__":
-    grid = ws.load_grid('jgrid10k.txt')
+    grid = ws.load_grid('jgrid1k.txt')
     jp = ws.load_words('日本.txt')
     matches = ws.find_words(ws._setscan_rtl(grid,jp), jp)
     print(len(matches))
